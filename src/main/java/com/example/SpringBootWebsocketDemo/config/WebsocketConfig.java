737bd43f62aa23b3663cc9292194aec91eccc3f9
JaxYoun <gao88jie@qq.com>
2018-08-15 18:30:52 +0800

    添加了机制注解

--- src/main/java/com/example/SpringBootWebsocketDemo/config/WebsocketConfig.java
+++ src/main/java/com/example/SpringBootWebsocketDemo/config/WebsocketConfig.java
@@ -12,18 +12,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  * @DateTime：2018/3/31 13:59
  */
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker  //启动websocket功能，默认为STOMP协议实现，所以启动的是STOMP over websocket功能
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");  //在endpoint中发布主题
+        config.setApplicationDestinationPrefixes("/app");  //在endpoint上监听指定前缀的客户端请求，用@MessageMapping的方法来处理请求
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();  //启动一个websocket端点服务，提供包括消息队列在内的websocket总体服务
     }
 
 }
